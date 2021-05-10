c70f5b58ddefdfe247eb487a5cf7f3bd160e5184
Cristian <llcristianaceroll@gmail.com>
2021-05-10 12:19:02 -0500

    Se agregaron plugins

--- website/assets/MediaPlayer.js
+++ website/assets/MediaPlayer.js
@@ -40,8 +40,15 @@
    
 function MediaPlayer (config) {
     this.media = config.el;
+    this.plugins = config.plugins || [];
+    this._initPlugins();
   }
 
+  MediaPlayer.prototype._initPlugins = function() {
+    this.plugins.forEach(plugin => {
+        plugin.run(this);
+    });
+  };
   MediaPlayer.prototype.play = function () {
     this.media.play();
   };
@@ -57,5 +64,14 @@ function MediaPlayer (config) {
       this.pause();
     }
   }; 
+
+  MediaPlayer.prototype.mute =  function() {
+    this.media.muted = true;
+  }; 
+
+  MediaPlayer.prototype.unmute =  function() {
+    this.media.muted = false;
+  }; 
+
 
   export default MediaPlayer;
